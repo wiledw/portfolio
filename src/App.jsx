640f9464c32ba1d9e8340c0a6b473b33ac62d0e8
William <william.gunawan@torontomu.ca>
2024-04-26 18:36:01 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./App.css";
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+
+    <main className='main'>
+      <Home/>
+    </main>
+    </>
+  )
+}
+
+export default App
+
