bad803a5a0b5f10850f53cc22e21457df182625e
William <william.gunawan@torontomu.ca>
2024-04-26 20:31:56 -0400

    add: about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./App.css";
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
-
+import About from './components/About/About';
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
 
     <main className='main'>
       <Home/>
+      <About/>
     </main>
     </>
   )
---
+++ src/components/About/About.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import "./about.css";
+import Resume from "../../assets/williamResume.pdf";
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+      <h2 className="section__title">README.md</h2>
+      <div className="container row" id="img-console">
+        <div id="console">
+          <span className="first-color">package </span>
+          <span className="fifth-color">aboutMe</span><span className="second-color">;</span>
+          <br/>
+          <br/>
+          <span className="first-color">public </span>
+          <span className="third-color">class </span>
+          <span className="fifth-color">William </span>
+          <span className="first-color">extends </span>
+          <span className="fifth-color">human </span>
+          <span className="second-color">{'{'}</span>
+          <br/>
+          <ul>
+            <span className="first-color">public </span>
+            <span className="fifth-color">William</span>
+            <span className="second-color">() {'{'}</span>
+            <br/>
+            <ul>
+              <span className="third-color">String </span>
+              <span className="second-color">fullName = </span>
+              <span className="fourth-color">"William Edwardo Gunawan"</span><span className="second-color">;</span>
+              <br/>
+              <span className="third-color">String </span>
+              <span className="second-color">location = </span>
+              <span className="fourth-color">"Toronto, ON, Canada"</span><span className="second-color">;</span>
+              <br/>
+              <span className="third-color">Resume </span>
+              <span className="second-color">resume = </span>
+              <span className="first-color">new </span>
+              <span className="second-color">Resume(</span>
+              <a download="" href={Resume} className="fourth-color">"Download-Resume"</a>
+              <span className="second-color">);</span>
+              <br/>
+              <span className="third-color">String[] </span>
+              <span className="second-color">hobbiesAndInterests = </span>
+              <span className="first-color">new </span>
+              <span className="third-color">String[]</span>
+              <span className="second-color">{'{'}</span>
+              <br/>
+              <ul>
+                <span className="fourth-color">"Coding"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Artificial Intelligence"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Eat"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Music"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Games"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Travel"</span>
+                <br/>
+              </ul>
+              <span className="second-color">{'}'}</span>
+              <br/>
+            </ul>
+            <span className="second-color">{'}'}</span>
+            <br/>
+            <span className="first-color">public static </span>
+            <span className="third-color">void </span>
+            <span className="fifth-color">Education</span>
+            <span className="second-color">() {'{'}</span>
+            <ul>
+              <span className="third-color">String </span>
+              <span className="second-color">university = </span>
+              <span className="fourth-color">"Toronto Metropolitan University"</span><span className="second-color">;</span>
+              <br/>
+              <span className="third-color">String </span>
+              <span className="second-color">degree = </span>
+              <span className="fourth-color">Bachelor of Science</span><span className="second-color">;</span>
+              <br/>
+              <span className="third-color">String </span>
+              <span className="second-color">major = </span>
+              <span className="fourth-color">Computer Science</span><span className="second-color">;</span>
+              <br/>
+              <span className="third-color">int </span>
+              <span className="second-color">expGraduation = </span>
+              <span className="fourth-color">May 2025</span><span className="second-color">;</span>
+              <br/>
+              <span className="third-color">Email </span>
+              <span className="second-color">email = </span>
+              <span className="first-color">new </span>
+              <span className="second-color">Email(</span>
+              <span className="fourth-color">"william.gunawan@torontomu.ca"</span>
+              <span className="second-color">);</span>
+              <br/>
+              <span className="third-color">String[] </span>
+              <span className="second-color">coursework = </span>
+              <span className="first-color">new </span>
+              <span className="third-color">String[]</span>
+              <span className="second-color">{'{'}</span>
+              <br/>
+              <ul>
+                <span className="fourth-color">"Data Structures and Algorithms"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Databases"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Software Engineering"</span>
+                <br/>
+                <span className="fourth-color">"Data Science"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Artificial Intelligence"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Machine Learning"</span><span className="second-color">,</span>
+                <br/>
+                <span className="fourth-color">"Web Design and Development"</span>
+                <br/>
+                <span className="fourth-color">"Operating Systems"</span>
+                <br/>
+                <span className="fourth-color">"Computer Architecture"</span>
+                <br/>
+              </ul>
+        
+             
+              <span className="second-color">{'}'}</span>
+            </ul>
+            <span className="second-color">{'}'}</span>
+            <br/>
+          </ul>
+          <span className="second-color">{'}'}</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
+
